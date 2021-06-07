897f68b1001ad276029817cf48ef6aa63ce52814
Federvgh <caffarofederico@gmail.com>
2021-06-07 21:29:48 +0000

    add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
+
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $("select").formSelect();
     $(".datepicker").datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
